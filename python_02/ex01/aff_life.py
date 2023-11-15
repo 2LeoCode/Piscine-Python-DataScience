46628a8cedd1038c459e8a60b32390ae4a46fc46
2LeoCode <leo.13suardi@outlook.com>
2023-11-15 12:55:55 +0100

    python_02/ex01

---
+++ python_02/ex01/aff_life.py
@@ -0,0 +1,25 @@
+import load_csv as csv
+from matplotlib import pyplot as plt
+import numpy as np
+
+
+def aff_life():
+    """
+    Plot the life expectancy projections for France.
+    """
+    data = csv.load("life_expectancy_years.csv")
+
+    x = data.columns.values[1:].astype(np.int16)
+    y = data.query("country == 'France'").values[0, 1:].astype(np.float32)
+
+    plt.plot(x, y)
+    plt.title("France Life expectancy Projections")
+    plt.xlabel("Year")
+    plt.ylabel("Life expectancy")
+    plt.show()
+
+
+if __name__ == "__main__":
+    aff_life()
+
+__all__ = "aff_life",
